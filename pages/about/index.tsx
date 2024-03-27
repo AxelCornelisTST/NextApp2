ea443e016463b63f2edbaef4a9011d8e0c93958b
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-03-27 22:29:55 +0100

    implement basic databse communication in pages for try-outs

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,6 +1,24 @@
+import QRScanner from 'qr-scanner';
+import {useState} from "react";
+
 export default function About() {
 
+    const [running, setRunning] = useState(false);
+    let videoElement: HTMLVideoElement = (document.getElementById('video-source') as HTMLVideoElement);
+
+    function update() {
+
+    }
+
+    function toggle() {
+        setRunning(!running)
+    }
+
     return (
-        <div>about</div>
+        <div>
+            <video id="video-source" className="rounded-s shadow-m"></video>
+            <button onClick={toggle}></button>
+        </div>
+
     );
 }
--- pages/info/index.tsx
+++ pages/info/index.tsx
@@ -1,16 +1,37 @@
-import getAllLaptops from "@/app/data";
-import {useState} from "react";
+import LaptopRepository from "@/app/repository/LaptopRepository";
+import {ILaptop} from "@/app/models/ILaptop";
+import {GetServerSideProps, InferGetServerSidePropsType} from "next";
 
-export default async function Info() {
+/**
+ * getServerSideProps is a function that gets called every request (but only on the server!), and it can pass data to the page that you can then use for rendering.
+ */
+export const getServerSideProps = (async () => {
 
-    const [fetch, setFetch] = useState(getAllLaptops())
+    const laptops = await LaptopRepository.retrieveAll();
+
+    return {
+        props: {
+            laptops
+        }
+    };
+
+}) satisfies GetServerSideProps<{ laptops: ILaptop[] }>
+
+export default function Info({laptops}: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
     return (
         <div>
-            hi
-            <div>
-                {fetch}
-            </div>
+            {
+                laptops.map((laps, index) =>
+                    <div key={index}>
+                        <p>{index}</p>
+                        <p>{laps.SerialNumber}</p>
+                        <p>{laps.Brand}</p>
+                        <p>{laps.Model}</p>
+                        <p>{laps.Processor}</p>
+                    </div>
+                )
+            }
         </div>
     );
 }
