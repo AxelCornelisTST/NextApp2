03c01d2fa4bba1311bf34bc6eafe92f8fbd5677e
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-03-28 20:29:33 +0100

    clear about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,24 +1,9 @@
-import QRScanner from 'qr-scanner';
-import {useState} from "react";
+import {JSX, useEffect, useRef, useState} from "react";
 
 export default function About() {
 
-    const [running, setRunning] = useState(false);
-    let videoElement: HTMLVideoElement = (document.getElementById('video-source') as HTMLVideoElement);
-
-    function update() {
-
-    }
-
-    function toggle() {
-        setRunning(!running)
-    }
-
     return (
         <div>
-            <video id="video-source" className="rounded-s shadow-m"></video>
-            <button onClick={toggle}></button>
         </div>
-
     );
 }
