57615356a3b0c5b751917ce9476e6a5a8692104c
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-04-05 02:42:51 +0200

    id pages will now show all laptop data and the posbility to add a user (not functional yet)

--- app/info/[id]/page.tsx
+++ app/info/[id]/page.tsx
@@ -1,38 +1,30 @@
+"use server"
 import LaptopRepository from "@/common/repository/LaptopRepository";
+import LaptopForm from "@/components/LaptopForm";
+import UserRepository from "@/common/repository/UserRepository";
+import {IUser} from "@/common/models/IUser";
+import BackButton from "@/components/backbutton";
 
 export default async function InfoID({params}: { params: { id: string } }) {
     const laptop = await LaptopRepository.retrieveBy(params.id);
-    return (
-        <main className="flex flex-col items-center justify-between p-24">
-            <h1 className="flex items-center">Laptop Info</h1>
+    let user: IUser | undefined
+    if (laptop)
+        user = await UserRepository.retrieveBy(laptop.UserID);
 
-            <table className="border-separate border-spacing-2 border border-slate-500">
-                <thead>
-                <tr>
-                    <th className="border border-slate-600 w-20">Info</th>
-                    <th className="border border-slate-600 w-40">Laptop Specs</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td className="border border-slate-700">s/n</td>
-                    <td className="border border-slate-700">{laptop?.SerialNumber}</td>
-                </tr>
-                <tr>
-                    <td className="border border-slate-700">merk</td>
-                    <td className="border border-slate-700">{laptop?.Brand}</td>
-                </tr>
-                <tr>
-                    <td className="border border-slate-700">model</td>
-                    <td className="border border-slate-700">{laptop?.Model}</td>
-                </tr>
-                <tr>
-                    <td className="border border-slate-700">core</td>
-                    <td className="border border-slate-700">{laptop?.Processor}</td>
-                </tr>
-                </tbody>
-            </table>
-        </main>
+    return (
+        <div className="flex flex-col items-center justify-between p-10">
+            <h1 className="flex items-center font-bold text-xl">Laptop Info</h1>
+            {
+                laptop
+                    ? <LaptopForm laptop={laptop} user={user}/>
+                    : <div className={"flex flex-col items-center"}>
+                        <p className={"error font-bold text-red-700 py-10 text-center"}>Laptop {params.id} does not exist in
+                            the database </p>
+                        <BackButton/>
+                    </div>
+            }
+
+        </div>
     )
 }
 
