21ea41e9e5f2ab73037dcc2c83dd2ee335af7b37
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-04-08 11:21:08 +0200

    add homepage session 'detector' add language entry for said 'detector'

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,15 +1,26 @@
 "use server"
 import Image from "next/image";
 import TranslateServer from "@/components/TranslateServer";
+import {getServerSession} from "next-auth";
+import {getSessionName} from "@/common/sessionconverter";
 
 export default async function Home({params}: { params: { lang: string } }) {
+    const session = await getServerSession()
+
+    const name: string = getSessionName(session);
 
     return (
         <div className="items-center flex flex-col justify-between">
             <TranslateServer lang={params.lang} text={'welcome'} className={"py-10 font-bold text-4xl"}/>
             <Image src={"/img.png"} alt={"laptop icon"} width={512} height={512} className={"w-fit"}/>
-            <TranslateServer lang={params.lang} text={'homepage_login_request'}
-                             className={"py-10 text-xl text-center"}/>
+            {
+                session ?
+                    <TranslateServer lang={params.lang} text={'homepage_greeting'} variables={{name}}
+                                     className={"px-10 py-5 text-xl text-center"}/>
+                    :
+                    <TranslateServer lang={params.lang} text={'homepage_login_request'}
+                                     className={"py-10 text-xl text-center"}/>
+            }
         </div>
     );
 }
---
+++ common/sessionconverter.ts
@@ -0,0 +1,19 @@
+import {Session} from "next-auth";
+
+export function getSessionMail(ses: Session | null): string {
+    if (!!ses && !!ses.user && !!ses.user.email)
+        return ses.user.email;
+    return "";
+}
+
+export function getSessionImg(ses: Session | null): string {
+    if (!!ses && !!ses.user && !!ses.user.image)
+        return ses.user.image;
+    return "";
+}
+
+export function getSessionName(ses: Session | null): string {
+    if (!!ses && !!ses.user && !!ses.user.name)
+        return ses.user.name;
+    return "user";
+}
