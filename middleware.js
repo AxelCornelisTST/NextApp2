b5c329a090bf3927e2007f3f454a04acbbf263fb
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-04-08 14:04:48 +0200

    remove disfunctional middleware

--- middleware.js
+++ middleware.js
@@ -1,16 +1,16 @@
-import {NextResponse} from 'next/server'
+import {NextRequest, NextResponse} from 'next/server'
 import acceptLanguage from 'accept-language'
 import {cookieName, fallbackLng, languages} from './app/i18n/settings'
 import {loginPage} from "./components/routerhelper";
+import {getSession} from "next-auth/react";
 
 acceptLanguage.languages(languages)
 
 export const config = {
-    // matcher: '/:lng*'
-    matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)']
+    matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)']
 }
 
-export function middleware(req) {
+export async function middleware(req) {
     //localisation
     let lang
     if (req.cookies.has(cookieName)) lang = acceptLanguage.get(req.cookies.get(cookieName).value)
@@ -29,13 +29,6 @@ export function middleware(req) {
         if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
         return response
     }
-
-    //authorisation
-    const currentUser = req.cookies.get('currentUser')?.value
-
-    const noLang = req.nextUrl.pathname.substring(3);
-    if (!currentUser && (noLang !== "" && !noLang.includes("about")))
-        return NextResponse.redirect(new URL(loginPage(), req.nextUrl));
 
     return NextResponse.next()
 }
