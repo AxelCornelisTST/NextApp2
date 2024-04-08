e541dcde653072386c33176084ee5ecc83cb9bf5
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-04-08 14:08:26 +0200

    add typeorm database adapter for connection

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,48 @@
-import NextAuth from "next-auth"
 import Github from "next-auth/providers/github";
+import NextAuth, {AuthOptions} from "next-auth";
+import {TypeORMAdapter} from "@auth/typeorm-adapter";
+import {Adapter} from "next-auth/adapters";
+import {ConnectionOptions} from "typeorm";
+import {SnakeNamingStrategy} from 'typeorm-naming-strategies'
 
 const githubClientId = process.env.GITHUB_ID;
 const githubClientSecret = process.env.GITHUB_SECRET;
+const server = process.env.DB_CONN;
 if (!githubClientId || !githubClientSecret) {
     throw new Error('GitHub credentials are not provided');
 }
+if (!server)
+    throw new Error('database connection not provided')
 
-const config = {
+const connection: ConnectionOptions = {
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "inventory",
+    namingStrategy: new SnakeNamingStrategy(),
+    synchronize: false
+}
+const config: AuthOptions = {
     providers: [
-        Github({clientId: githubClientId, clientSecret: githubClientSecret})
+        Github({
+            clientId: githubClientId,
+            clientSecret: githubClientSecret,
+        })
     ],
+    adapter: TypeORMAdapter(connection) as Adapter,
+    session: {
+        // Choose how you want to save the user session.
+        // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+        // If you use an `adapter` however, we default it to `"database"` instead.
+        // You can still force a JWT session by explicitly defining `"jwt"`.
+        // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+        // which is used to look up the session in the database.
+        strategy: "database",
+        //expressed in seconds
+        maxAge: 30 //Seconds
+    }
 }
 
 const handler = NextAuth(config)
