bc005b39f1b1c3fbc6a8fe6e33c04e93f91cd190
Axel Cornelis PXL <axel.cornelis@student.pxl.be>
2024-04-15 16:03:55 +0200

    first commit with surface changes, following documentation on https://lucia-auth.com/getting-started/nextjs-app

---
+++ app/auth.ts
@@ -0,0 +1,43 @@
+import {Lucia} from "lucia";
+import {Mysql2Adapter} from "@lucia-auth/adapter-mysql";
+import {dataBasePoolConnection} from "@/common/database";
+
+const adapter = new Mysql2Adapter(dataBasePoolConnection, {
+    user: "user", //user scheme name
+    session: "user_session" //session scheme name
+});
+
+export const lucia = new Lucia(adapter, {
+    sessionCookie: {
+        // this sets cookies with super long expiration
+        // since Next.js doesn't allow Lucia to extend cookie expiration when rendering pages
+        expires: false,
+        attributes: {
+            // set to `true` when using HTTPS
+            secure: process.env.NODE_ENV === "production"
+        }
+    },
+    getUserAttributes: (attributes) => {
+        return {
+            // attributes has the type of DatabaseUserAttributes
+            role: attributes.role,
+            githubId: attributes.github_id,
+            username: attributes.username
+        };
+    }
+});
+
+// IMPORTANT!
+declare module "lucia" {
+    interface Register {
+        Lucia: typeof lucia;
+        DatabaseUserAttributes: DatabaseUserAttributes;
+
+    }
+}
+
+interface DatabaseUserAttributes {
+    username: string;
+    role: string;
+    github_id: number;
+}
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,23 +1,23 @@
-import "next-auth";
+// import "next-auth";
+// //
+// declare module "next-auth" {
 //
-declare module "next-auth" {
-
-    interface User {
-        role: string;
-    }
-
-    /**
-     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-     */
-    interface Session {
-        user?: {
-            role: string
-        } & DefaultSession["user"];
-    }
-}
-
-declare module "next-auth/jwt" {
-    interface JWT {
-        role: "SUPER" | "USER";
-    }
-}
+//     interface User {
+//         role: string;
+//     }
+//
+//     /**
+//      * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+//      */
+//     interface Session {
+//         user?: {
+//             role: string
+//         } & DefaultSession["user"];
+//     }
+// }
+//
+// declare module "next-auth/jwt" {
+//     interface JWT {
+//         role: "SUPER" | "USER";
+//     }
+// }
