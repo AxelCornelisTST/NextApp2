b6037a58d3552f9f5c466cc56da4feda93014e1b
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-04-30 00:43:33 +0200

    vvariable name refactor to prevent confusion with ormConfig

--- app/[lang]/info/[id]/page.tsx
+++ app/[lang]/info/[id]/page.tsx
@@ -3,10 +3,11 @@ import LaptopForm from "@/components/LaptopForm";
 import BackButton from "@/components/BackButton";
 import TranslateServer from "@/components/TranslateServer";
 import {getServerSession} from "next-auth";
-import {authOptions, databaseSource} from "@/app/api/auth/[...nextauth]/route";
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 import AccessDenied from "@/components/AccessDenied";
 import {LaptopEntity} from "@/common/models/ILaptop";
 import {LaptopUserEntity} from "@/common/models/ILaptopUser";
+import {migrationDataSource} from "@/ormconfig";
 
 export default async function Page({params}: { params: { lang: string, id: string } }) {
     const session = getServerSession(authOptions);
@@ -15,10 +16,10 @@ export default async function Page({params}: { params: { lang: string, id: strin
 
     const id = params && !Array.isArray(params.id) ? params.id : "err";
     const lang = params && !Array.isArray(params.lang) ? params.lang : "en"
-    const laptop = await databaseSource.getRepository(LaptopEntity).findOneBy({serialNumber: id});
+    const laptop = await migrationDataSource.getRepository(LaptopEntity).findOneBy({serialNumber: id});
     let user = undefined
     if (laptop)
-        user = await databaseSource.getRepository(LaptopUserEntity).findOneBy({userID: laptop.laptopUser.userID});
+        user = await migrationDataSource.getRepository(LaptopUserEntity).findOneBy({userID: laptop.laptopUser.userID});
     return (
         <div className="flex flex-col items-center justify-between p-10">
             <h1 className="flex items-center font-bold text-xl">Laptop Info</h1>
