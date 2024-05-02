e18cf842e30f5c2557763a845eaacad5c788309f
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-05-02 23:54:46 +0200

    * add infocard to info ID page

--- app/[lang]/info/[id]/page.tsx
+++ app/[lang]/info/[id]/page.tsx
@@ -1,5 +1,4 @@
 "use server"
-import LaptopForm from "@/components/LaptopForm";
 import BackButton from "@/components/BackButton";
 import TranslateServer from "@/components/TranslateServer";
 import {getServerSession} from "next-auth";
@@ -7,26 +6,40 @@ import {authOptions, databaseSource} from "@/app/api/auth/[...nextauth]/route";
 import AccessDenied from "@/components/AccessDenied";
 import {LaptopEntity} from "@/common/models/ILaptop";
 import {LaptopUserEntity} from "@/common/models/ILaptopUser";
-import {migrationDataSource} from "@/ormconfig";
+import InfoCard from "@/components/cards/InfoCard";
 
 export default async function Page({params}: { params: { lang: string, id: string } }) {
     const session = getServerSession(authOptions);
     if (!session)
         return <AccessDenied lang={params.lang}/>
 
+    if (!databaseSource.isInitialized)
+        await databaseSource.initialize()
+
     const id = params && !Array.isArray(params.id) ? params.id : "err";
     const lang = params && !Array.isArray(params.lang) ? params.lang : "en"
     const laptop = await databaseSource.getRepository(LaptopEntity).findOneBy({serialNumber: id});
     let user = undefined
-    if (laptop)
+    if (laptop && laptop.laptopUser)
         user = await databaseSource.getRepository(LaptopUserEntity).findOneBy({userID: laptop.laptopUser.userID});
     return (
         <div className="flex flex-col items-center justify-between p-10">
             <h1 className="flex items-center font-bold text-xl">Laptop Info</h1>
             {
                 laptop
-                    ? <LaptopForm lang={lang} laptop={laptop} user={user}/>
-                    : <div className={"flex flex-col items-center"}>
+                    ?
+                    <div className={"grid grid-cols-1 gap-2 backdrop-blur-sm pb-5"}>
+                        <InfoCard lang={params.lang}
+                                  laptop={{
+                                      sn: laptop.serialNumber,
+                                      brand: laptop.brand,
+                                      details: laptop.processor,
+                                      model: laptop.model
+                                  }} user={user ? {name: user.firstName, famName: user.firstName} : undefined}
+                        />
+                    </div>
+                    :
+                    <div className={"flex flex-col items-center"}>
                         <div className={"error font-bold text-red-700 py-10 text-center"}>
                             <TranslateServer lang={lang} text={"no_laptop_in_database"} variables={{id}}/>
                         </div>
